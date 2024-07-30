a03d6b6f5eb22ddd5dde394a8f76b61b62a6074d
potchie <archie.workmatter@gmail.com>
2024-07-30 21:14:23 +0800

    front page

--- resources/views/content/authentications/auth-login-basic.blade.php
+++ resources/views/content/authentications/auth-login-basic.blade.php
@@ -17,14 +17,13 @@
           <!-- Logo -->
           <div class="app-brand justify-content-center">
             <a href="{{url('/')}}" class="app-brand-link gap-2">
-              <span class="app-brand-logo demo">@include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])</span>
-              <span class="app-brand-text demo text-body fw-bold">{{config('variables.templateName')}}</span>
+              <span class="app-brand-logo demo">
+              <img src="{{asset('assets/img/index/agds_w.jpg')}}">
+              </span>
             </a>
           </div>
           <!-- /Logo -->
-          <h4 class="mb-2">Welcome to {{config('variables.templateName')}}! 👋</h4>
-          <p class="mb-4">Please sign-in to your account and start the adventure</p>
-
+  
           <form id="formAuthentication" class="mb-3" action="{{url('/')}}" method="GET">
             <div class="mb-3">
               <label for="email" class="form-label">Email or Username</label>
--- resources/views/layouts/sections/styles.blade.php
+++ resources/views/layouts/sections/styles.blade.php
@@ -9,7 +9,6 @@
 <!-- Core CSS -->
 <link rel="stylesheet" href="{{ asset(mix('assets/vendor/css/core.css')) }}" />
 <link rel="stylesheet" href="{{ asset(mix('assets/vendor/css/theme-default.css')) }}" />
-<link rel="stylesheet" href="{{ asset(mix('assets/css/demo.css')) }}" />
 <!-- Vendors CSS -->
 <link rel="stylesheet" href="{{ asset(mix('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')) }}" />
 
