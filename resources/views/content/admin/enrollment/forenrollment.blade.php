67f5bc246062f8f265914e8aba8a33fa646b26e4
potchie <mobile.potchie@gmail.com>
2024-11-08 14:12:07 +0800

    fixing the approval of enrollment

--- resources/views/content/admin/enrollment/forenrollment.blade.php
+++ resources/views/content/admin/enrollment/forenrollment.blade.php
@@ -14,8 +14,10 @@
                 <th style="text-align:center;">Student's Full name</th>
                 <th style="text-align:center;">Course</th>
                 <th style="text-align:center;">Category</th>
-                <th style="text-align:center;">Schedule</th>
-                <th style="text-align:center;">Trainor</th>
+                <th style="text-align:center;">Duration</th>
+                <th style="text-align:center;">Timeslot</th>
+                <th style="text-align:center;">Trainer</th>
+                <th style="text-align:center;">Reference No.</th>
                 <th style="text-align:center;">Action</th>
             </tr>
         </thead>
@@ -24,8 +26,12 @@
                 <tr style="text-align:center;">
                     <td>{{$enrollment->user->fname}} {{$enrollment->user->mname}} {{$enrollment->user->lname}}</td>
                     <td>{{$enrollment->classSchedule->course->name}}</td>
-                    <td>{{$enrollment->classSchedule->course->category}}</td>
+                    <td>{{$enrollment->classSchedule->category->name}}</td>
+                    <td>{{date('M. d, Y',strtotime($enrollment->classSchedule->day_start))}}-{{date('M. d, Y',strtotime($enrollment->classSchedule->day_end))}}</td>
+                    <td>{{date('h:s a',strtotime($enrollment->classSchedule->time_start))}}-{{date('h:s a',strtotime($enrollment->classSchedule->time_end))}}</td>
                     <td>{{$enrollment->classSchedule->user->fname }} {{$enrollment->classSchedule->user->mname }} {{$enrollment->classSchedule->user->lname }}</td>
+                    <td>{{$enrollment->referenceNo}}</td>
+                    <td><a href='#'><i class='bx bx-check-circle' ></i></a> | <a href='#'><i class='bx bxs-x-circle'></i></a></td>
                 </tr>
             @endforeach
         </tbody>
